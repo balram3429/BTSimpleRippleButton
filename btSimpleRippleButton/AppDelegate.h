4319db64fa0de03f4ad2f8832beb82b1e96f2c0d
balram tiwari <balram.tiwari@photoninfotech.net>
2014-06-01 09:44:10 +0530

    Initial Commit

---
+++ btSimpleRippleButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  btSimpleRippleButton
+//
+//  Created by Balram Tiwari on 01/06/14.
+//  Copyright (c) 2014 Balram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ btSimpleRippleButton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  btSimpleRippleButton
+//
+//  Created by Balram Tiwari on 01/06/14.
+//  Copyright (c) 2014 Balram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
